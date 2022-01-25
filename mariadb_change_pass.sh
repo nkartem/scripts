2c10925280e22a7cbf14bf2979507f6d293db332
Artem <boss_aaa@ukr.net>
2022-01-25 17:23:07 +0200

    Update mariadb_change_pass.sh

--- mariadb_change_pass.sh
+++ mariadb_change_pass.sh
@@ -5,7 +5,8 @@ sudo systemctl set-environment MYSQLD_OPTS="--skip-grant-tables --skip-networkin
 sudo systemctl start mysql
 # change password
 sudo mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('mysql');"
-mysql -u root -pmysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('1123456');"
+mysql -u root -pmysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('zxcTYU057@#');"
+mysql -u root -pzxcTYU057@\# -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('mysql');"
 #Enter password:
 #0300
 #SET PASSWORD FOR 'root'@'localhost' = PASSWORD('111111');
