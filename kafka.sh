c26244bcae945eb90a2d4813a1143133a18570af
Artem <boss_aaa@ukr.net>
2022-02-02 17:18:05 +0200

    Update kafka.sh

--- kafka.sh
+++ kafka.sh
@@ -9,7 +9,9 @@
 sudo subscription-manager repos --disable rhel-8-for-x86_64-appstream-eus-source-rpms
 sudo dnf update -y
 sudo dnf -y install epel-release
-sudo dnf -y install java-11-openjdk
+cd /tmp
+sudo wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
+sudo rpm -ivh jdk-17_linux-x64_bin.rpm
 cd /tmp
 sudo wget https://dlcdn.apache.org/kafka/3.1.0/kafka_2.13-3.1.0.tgz
 tar xzf kafka_2.13-3.1.0.tgz
@@ -35,11 +37,11 @@ sudo tee /etc/systemd/system/kafka.service <<EOF
 Description=Apache Kafka Server
 Documentation=http://kafka.apache.org/documentation.html
 Requires=zookeeper.service
-#After=zookeeper.service #for CentOS8
+After=zookeeper.service
 
 [Service]
 Type=simple
-Environment="JAVA_HOME=/usr/lib/jvm/jre-11-openjdk"
+#Environment="JAVA_HOME=/usr/lib/jvm/jre-11-openjdk"
 ExecStart=/usr/bin/bash /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties
 ExecStop=/usr/bin/bash /usr/local/kafka/bin/kafka-server-stop.sh
 
@@ -52,15 +54,15 @@ sudo systemctl enable kafka
 sudo systemctl start zookeeper
 sudo systemctl start kafka
 sudo firewall-cmd --zone=public --permanent --add-port 9092/tcp
+sudo firewall-cmd --zone=public --permanent --add-port 8080/tcp
 sudo firewall-cmd --zone=public --permanent --add-port 2181/tcp
 sudo firewall-cmd --reload
 # sudo systemctl status kafka
 # sudo systemctl status zookeeper
 #------Fihish install Kafka-------#
 
-
 podman run --restart=always -p 8080:8080 \
 	-e KAFKA_CLUSTERS_0_NAME=local \
-	-e KAFKA_CLUSTERS_0_ZOOKEEPER=192.168.10.213 \
-	-e KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS=192.168.10.213:9092 \
+	-e KAFKA_CLUSTERS_0_ZOOKEEPER=192.168.10.161 \
+	-e KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS=192.168.10.161:9092 \
 	-d provectuslabs/kafka-ui:latest 
